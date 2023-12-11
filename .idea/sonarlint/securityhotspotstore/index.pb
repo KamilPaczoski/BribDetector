5120ad59b04645b73b56d8606aae0650338bc931
KamilPaczoski <kamil.paczoski13@gmail.com>
2023-12-11 10:00:06 +0100

    no comparision, pre test

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+7
+main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
+Å
+Q.idea/sonarlint/securityhotspotstore/f/1/f1bdda93d9a278e358509d498e17d97764c1fb29,b\5\b58a0677868503201074e965423d875803239017
+Å
+Q.idea/sonarlint/securityhotspotstore/1/e/1e9075f5bf079c01ef2c910709e91a497d262080,2\b\2b8cdd31beed7f49883db0988fe57e4618cd62fd
+Å
+Q.idea/sonarlint/securityhotspotstore/4/9/496a238a6afa168dbaf6efd37bb459331589579c,b\0\b0bb0a26d88d16d2e82502fe47e034e248a527a2
+w
+G.idea/sonarlint/issuestore/1/e/1e9075f5bf079c01ef2c910709e91a497d262080,2\5\2511a7590eb091d560dac1615e6bcef0ceeaffb6
+w
+G.idea/sonarlint/issuestore/4/9/496a238a6afa168dbaf6efd37bb459331589579c,6\e\6e9644ddf83ab52f39902ecdbfdf4118285ac40c
--- main.py
+++ main.py
@@ -1,2 +1,83 @@
 from ultralytics import YOLO
 import cv2
+import os
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+
+
+class BirbDetector:
+    fps = 30
+    resolution = (654, 480)
+
+    def __init__(self):
+        self.purge()
+        self.video_splitter()
+        self.hand_drawing()
+        self.yolo()
+        self.editing_video()
+        self.video_merger()
+
+
+    @staticmethod
+    def purge():
+        try:
+            for file in os.listdir('frames'):
+                os.remove('frames/' + file)
+            os.remove('edited_video.mp4')
+        except FileNotFoundError:
+            pass
+
+    def video_splitter(self):
+        vc = cv2.VideoCapture('video.mp4')
+        vc.set(cv2.CAP_PROP_FPS, self.fps)
+        c = 0
+        read = vc.isOpened()
+        while read:
+            read, frame = vc.read()
+            cv2.imwrite('frames/' + str(c) + '.jpg', frame)
+            c += 1
+        vc.release()
+
+    def video_merger(self):
+        video = cv2.VideoWriter('edited_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), self.fps, self.resolution)
+        for i in range(0, len(os.listdir('frames'))):
+            img = cv2.imread('frames/' + str(i) + '.jpg')
+            video.write(img)
+
+        video.release()
+
+    @staticmethod
+    def yolo():
+        model = YOLO("yolov8n.pt")
+        model.train(data='coco128.yaml', epochs=4, imgsz=640, patience=2, batch=16, workers=8, device='0')
+        model.predict()
+        model.save('yolov8n.pt')
+        output = model('frames/0.jpg')
+        print(output.xyxy[0])
+        print("__________________________")
+        print(model.val())
+
+    @staticmethod
+    def hand_drawing():
+        img = Image.open('frames/0.jpg')
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('arial.ttf', 25)
+        draw.rectangle(((155, 180), (260, 290)), None, 'purple', 2)
+        draw.rectangle(((400, 255), (500, 370)), None, 'green', 2)
+        draw.rectangle(((570, 120), (650, 200)), None, 'orange', 2)
+        draw.text((0, 0), 'Three birbs', font=font)
+
+        img.save('frames_hand/0.jpg')
+
+    def editing_video(self):
+        vc = cv2.VideoCapture('video.mp4')
+        vc.set(cv2.CAP_PROP_FPS, self.fps)
+        model = YOLO("yolov8n.pt")
+        model.predict('frames', save_dir='frames_edited', save=True, conf=0.5)
+
+    def compare(self):
+        auto = Image.open('frames_edited/0.jpg')
+        hand = Image.open('frames_hand/0.jpg')
+
+
+if __name__ == '__main__':
+    BirbDetector()

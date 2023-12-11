e9ca44dc54aead469f9729f9233f4d78fda1d1ca
KamilPaczoski <kamil.paczoski13@gmail.com>
2023-12-11 13:05:03 +0100

    Done, finished

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 import cv2
 import os
-from PIL import Image, ImageDraw, ImageFont, ImageColor
+from PIL import Image, ImageDraw, ImageFont
 
 
 class BirbDetector:
     fps = 30
-    resolution = (654, 480)
+    resolution = (850, 480)
 
     def __init__(self):
         self.purge()
@@ -15,7 +15,7 @@ class BirbDetector:
         self.yolo()
         self.editing_video()
         self.video_merger()
-
+        self.iou()
 
     @staticmethod
     def purge():
@@ -23,6 +23,7 @@ class BirbDetector:
             for file in os.listdir('frames'):
                 os.remove('frames/' + file)
             os.remove('edited_video.mp4')
+            os.remove('runs')
         except FileNotFoundError:
             pass
 
@@ -39,22 +40,17 @@ class BirbDetector:
 
     def video_merger(self):
         video = cv2.VideoWriter('edited_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), self.fps, self.resolution)
-        for i in range(0, len(os.listdir('frames'))):
-            img = cv2.imread('frames/' + str(i) + '.jpg')
+        for i in range(0, 200):
+            img = cv2.imread('runs/detect/predict/' + str(i) + '.jpg')
             video.write(img)
-
         video.release()
 
     @staticmethod
     def yolo():
         model = YOLO("yolov8n.pt")
-        model.train(data='coco128.yaml', epochs=4, imgsz=640, patience=2, batch=16, workers=8, device='0')
-        model.predict()
-        model.save('yolov8n.pt')
-        output = model('frames/0.jpg')
-        print(output.xyxy[0])
-        print("__________________________")
-        print(model.val())
+        model.train(data='coco128.yaml', epochs=3, imgsz=640, patience=2)
+
+        print(model('frames/0.jpg'))
 
     @staticmethod
     def hand_drawing():
@@ -65,18 +61,28 @@ class BirbDetector:
         draw.rectangle(((400, 255), (500, 370)), None, 'green', 2)
         draw.rectangle(((570, 120), (650, 200)), None, 'orange', 2)
         draw.text((0, 0), 'Three birbs', font=font)
-
         img.save('frames_hand/0.jpg')
 
     def editing_video(self):
         vc = cv2.VideoCapture('video.mp4')
         vc.set(cv2.CAP_PROP_FPS, self.fps)
         model = YOLO("yolov8n.pt")
-        model.predict('frames', save_dir='frames_edited', save=True, conf=0.5)
+        model.predict('frames', save_dir='frames_edited', save=True, save_txt=True, conf=0.5)
 
-    def compare(self):
-        auto = Image.open('frames_edited/0.jpg')
-        hand = Image.open('frames_hand/0.jpg')
+    def iou(self):
+        with open('runs/detect/predict3/labels/0.txt', 'r') as f:
+            lines = f.readlines()
+        a = self.resolution[0]
+        b = self.resolution[1]
+        for line in lines:
+            line = line.split(' ')
+            union_rect = [min(float(line[1]) * a, float(line[3]) * a), min(float(line[2]) * b, float(line[4]) * b),
+                          max(float(line[1]) * a, float(line[3]) * a), max(float(line[2]) * b, float(line[4]) * b)]
+        int_rect = [max(155, 400, 570), max(180, 255, 120), min(260, 500, 650), min(290, 370, 200)]
+        int_area = (int_rect[2] - int_rect[0]) * (int_rect[3] - int_rect[1])
+        union_area = (union_rect[2] - union_rect[0]) * (union_rect[3] - union_rect[1])
+        iou = int_area / union_area
+        print(iou)
 
 
 if __name__ == '__main__':
